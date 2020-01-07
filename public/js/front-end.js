ee58673e373cfa8b7b7e24ae8abb203aca99b1b2
Colin McCullough <colin.n.mccullough@gmail.com>
2020-01-06 16:42:22 -0800

    added more to redirects ui

--- public/js/front-end.js
+++ public/js/front-end.js
@@ -1,7 +1,3 @@
-let vertical = $("select[id='vertical']")
-let domainStrat = $("select[id='domain-strat']")
-let chainBranding = $("select[id='chain-brand']")
-
 //insert table headers based on vertical selection
 $("select[id='vertical']").change( function() {
   const vertical = $(this).val()
@@ -19,9 +15,11 @@ $("select[id='vertical']").change( function() {
 
 /////Table row functions, will need to be moved eventually
 //Event handler to delete table row in seo lv table
-$('#table').on('click', '.table-remove', function () {
-  $(this).parents('tr').detach();
-});
+const addDeleteEventHndlr = () => {
+  $('.table-remove').on('click', function () {
+    $(this).parents('tr').detach();
+  });
+}
 //Change Display from table row Drop downs on selection
 $(".dropdown-menu").on('click', 'a', function(){
   $(this).parents('.dropdown').find('button').text($(this).text());
--- public/js/redirects.js
+++ public/js/redirects.js
@@ -1,4 +1,6 @@
-$("#format-redirects").on('click', function() {
+$("#format-redirects").on('click', function() { buildRedirectTable() });
+
+const buildRedirectTable = () => {
     const locName = $("select[id='select-location']").val();
     const strategy = $("select[id='redirect-menu']").val();
     const redirects = $('#redirects-text').val()
@@ -6,10 +8,23 @@ $("#format-redirects").on('click', function() {
         window.alert('Select a location, strategy and enter redirects before continuing')
     }
     else {
-        const tablehtml = generateRedirectTable(locName,strategy,redirects)
-        $(".redirects-table-head").after( tablehtml )
+        const existingNumRows = $('.redirects-table-body tr').length;
+        const tablehtml = generateRedirectTable(locName,strategy,redirects);
+        $(".redirects-table-body").append( tablehtml );
+        //loops through all rows if no rows initially
+        if(existingNumRows === 0) {
+            $('.redirects-table-body tr').each(function (i, row) {
+                $(row).find(`#${strategy}`).prop('selected',true)
+            })
+        //loops through newly added rows existing rows in table
+        } else {
+            $(`.redirects-table-body tr:gt(${existingNumRows-1})`).each(function (i, row) {
+                $(row).find(`#${strategy}`).prop('selected',true);
+            });
+        }
+        addDeleteEventHndlr();
     }
-  });
+}
 
 //checks to make sure location, strategy & redirects entered
 const checkRedirectState = (name,dropDown,text) => {
@@ -21,48 +36,77 @@ const generateRedirectTable = (name, strategy, redirects) => {
     //filters falsy values, removes duplicates
     const array = redirects.split(/\n|,/g);
     const redirectsArr = array.filter((item,index) => array.indexOf(item) === index)
-                            .filter(item => Boolean(item.trim()));
-    return buildTableHtml(redirectsArr,strategy,name)
-
+                            .filter(item => Boolean(item.trim())); //need to filter out redirects that have image files
+    return buildHtmlTable(redirectsArr,strategy,name)
 }
 
-const buildTableHtml = (arr,strategy,name) => {
+//builds string of all rows
+const buildHtmlTable = (arr,strategy,name) => {
     let table = ``
     arr.forEach((redirect) => {
-        table += tblrow(name,redirect)
+        const cloudRedirect = formatRedirect(strategy,redirect)
+        table += tblrow(name,redirect,cloudRedirect)
     })
     return table;
 }
 
-const tblrow = (name,url,strategy) => {
-    return `
-        <tbody>
-            <tr>
-                <td class="pt-3-half" contenteditable="true" id="name">${name}</td>
-                <td class="pt-3-half" contenteditable="true">
-                    <select id="strat" class="form-control form-control-lg" >
-                        <option value="same-dom">Same Domain</option>
-                        <option value="cross-dom">Cross Domain</option>
-                        <option value="sec-cross-dom">Secure Cross Domain</option>
-                        <option value="sec-naked" >Secure Naked - Same Domain</option>
-                        <option value="no-redirects">No Redirects</option>
-                    </select>
-                </td>
-                <td class="pt-3-half" contenteditable="true" id="current-url">${url}</td>
-                <td class="pt-3-half" contenteditable="true" id="cloud-formatted"></td>
-                <td class="pt-3-half" contenteditable="true">
-                    <select id="wildcard-stat" class="form-control form-control-lg" >
-                        <option value="same-dom">Yes</option>
-                        <option value="cross-dom">No</option>
-                    </select>
-                </td>
-                <td class="pt-3-half" contenteditable="true" id="status-code"></td>
-                <td class="pt-3-half" contenteditable="true" id="redirects-to"></td>
-                <td>
-                    <span class="table-remove">
-                        <button type="button" class="btn btn-danger btn-rounded btn-sm my-0">Remove</button>
-                    </span>
-                </td>
-            </tr>
-        </tbody>`
+//formats redirects
+const formatRedirect = (strategy,redirect) => {
+    let formatted = 'N/A'
+    if(strategy === 'same-dom' && validURL(redirect)) {
+        formatted = redirect.split(/\.com\/|\.com|\.net\/|\.org\/|\.co\//)[1]     //strips everthing left of the TLD (ex: www.myapartments.com/units -> units)
+        .split(/\.html|[?]/)[0];                //strips everything to the right of .html or ? 
+        formatted.substr(-1) === '/' ? formatted.pop() : '';
+        formatted = formatted.replace(/%20|\s/g,'\\s')
+            .replace(/[[\]{}()*+?.,^$|#]/g, '\\$&')            //escapes special characters with \
+            .replace(/\/\//g,'/+')                               //replaces // with /+
+            .replace(/%5B|%5b/g,'\\[')                                   
+            .replace(/%5D|%5d/g,'\\]')
+            .replace(/%7C/g,'\\|');   
+        formatted = formatted.length === 1 ? `[${formatted}]` : `${formatted}$`                 
     }
+    return formatted
+}
+
+//checks for a valid url
+const validURL = (str) => {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
+const tblrow = (name,url,redirect) => {
+    return `
+        <tr>
+            <td class="pt-3-half" contenteditable="true" id="name">${name}</td>
+            <td class="pt-3-half" contenteditable="true">
+                <select id="strat" class="form-control form-control-md >
+                    <option id="" select>Select Strategy</option>
+                    <option id="same-dom">Same Domain</option>
+                    <option id="cross-dom">Cross Domain</option>
+                    <option id="sec-cross-dom">Secure Cross Domain</option>
+                    <option id="sec-naked" >Secure Naked - Same Domain</option>
+                    <option id="no-redirects">No Redirects</option>
+                </select>
+            </td>
+            <td class="pt-3-half" contenteditable="true" id="current-url">${url}</td>
+            <td class="pt-3-half" contenteditable="true" id="cloud-formatted">${redirect}</td>
+            <td class="pt-3-half" contenteditable="true">
+                <select id="wildcard-stat" class="form-control form-control-md" >
+                    <option >No</option>
+                    <option >Yes</option>
+                </select>
+            </td>
+            <td class="pt-3-half" contenteditable="true" id="status-code"></td>
+            <td class="pt-3-half" contenteditable="true" id="redirects-to"></td>
+            <td>
+            <span class="table-remove"><button type="button"
+                class="btn btn-danger btn-rounded btn-sm my-0">Remove</button></span>
+            </td>
+        </tr>
+    `
+}
